1db0772b7880a3b0ba15f2627092c38cb6ed4f4e
Dheeraj Malvi <drjmalviya@gmail.com>
2026-06-09 16:58:31 +0530

    refactor: Remove commented-out code in Vite configuration for cleaner structure

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig(({ command }) => {
-//   return {
-//   base: command === 'build' ? '/students-freelance/' : '/',
-//   plugins: [react()],
-//   }
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
